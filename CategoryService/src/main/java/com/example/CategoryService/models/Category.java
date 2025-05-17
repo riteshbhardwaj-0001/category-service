acab1e4289a3604126dada56cb8002a485a5741e
Ritesh Bhardwaj <riteshbhardwaj00001@gmail.com>
2025-05-17 11:34:36 +0530

    Initial Commit

---
+++ CategoryService/src/main/java/com/example/CategoryService/models/Category.java
@@ -0,0 +1,19 @@
+package com.example.CategoryService.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends BaseEntity{
+
+    private String name;
+}
